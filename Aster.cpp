2d4e588c3da2bddaffded5c1245c8ee7a07d11f9
友輝 斎藤 <220188@jc-21.jp>
2023-11-28 11:34:33 +0900

    hozonsiwasur e

--- Aster.cpp
+++ Aster.cpp
@@ -2,7 +2,8 @@
 #include<cmath>
 #include "Aster.h"
 #include<deque>
-
+//https://stone-program.com/python/algorithm/a-star-introduction/
+//https://qiita.com/2dgames_jp/items/f29e915357c1decbc4b7
 const int nonPath = 0;//0は通れない場所！
 int Aster::heuristic(POS now)
 {
@@ -22,18 +23,18 @@ Aster::Aster()
 	};//各要素はコスト
 	
 
-	CellMap.resize(mapY);
+	/*CellMap.resize(mapY);
 	for (int i = 0; i < mapY; i++)
 	{
 		CellMap.at(i).resize(mapX, Cell());
-	}
-	for (int i = 0; i < mapY; i++)
+	}*/
+	/*for (int i = 0; i < mapY; i++)
 	{
 		for(int j=0;j<mapX;j++)
 		{
 			CellMap.at(i).at(j).cellpos = POS{ j,i };
 		}
-	}
+	}*/
 
 }
 
@@ -43,21 +44,34 @@ Aster::~Aster()
 
 void Aster::BeginSearch()
 {
-	CellMap.at(start_.y_).at(start_.x_).certainCost = 0;
+	//CellMap.at(start_.y_).at(start_.x_).certainCost = 0;
+	openCells.emplace(start_,Cell());
 	Search(start_);
 }
 
 void Aster::Search(POS pos_)
 {
-	std::deque<POS>poslist;//
+
 	//上下左右見る
 	{//上
-		int next = pos_.y_ - 1;
-		if (next >= 0 && map.at(next).at(pos_.x_) != nonPath);
+		POS next = { pos_.y_ - 1, pos_.x_};
+		if (next .y_>= 0 && map.at(next.y_).at(next.x_) != nonPath);
 		{
-			map.at(next).at
+			Cell &NextCell = CellMap.at(next.y_).at(next.x_);
+			int h = heuristic(next);
+			NextCell.estimationCost = h;
+			NextCell.certainCost = CellMap.at(pos_.y_).at(pos_.x_).certainCost + 1;
+
 		}
 	}
+
+
+
+
+
+
+//現在ノードがゴールノードだったら終了
+	//ゴールノードが見つからなかったら終了
 }
 void Aster::Show()
 {
--- Aster.h
+++ Aster.h
@@ -9,12 +9,11 @@ class Aster
 public:
 	struct POS { int x_; int y_; POS() :x_(-1), y_(-1) {} POS(int x, int y):x_(x),y_(y){} };
 	struct Cell {//いちます毎の情報
-		POS cellpos;
+		//POS cellpos;
 		//int cost;//そのセルを通るときのコスト
 		int certainCost;//実コスト
 		int estimationCost;//推定関数、実＋ひゅーり
 		Cell():/*cost(-1),*/certainCost(-1),estimationCost(-1) {}
-		Cell(int x, int y) :Cell() { cellpos = POS(x, y);}
 	/*	auto operator <=>(const Cell&)const {
 			
 			return
@@ -23,10 +22,11 @@ public:
 private:
 
 	vector < vector < int >> map;//通れるところとかコストとか
-	vector < vector < Cell >> CellMap;//情報入れておくもの
+	//vector < vector < Cell >> CellMap;//情報入れておくもの
 	int heuristic(POS now);//現在地からの最短経路
 	void Search(POS pos_);//肝要。再帰？
-
+	std::map<POS, Cell> openCells;
+	std::map<POS, Cell>closeCells;
 public:
 	POS target_;//探索目的地点
 	POS start_;//探索開始地点
